da84b1cdaf9faf7d40e147ea344224d067e076bc
Igor <igor_stm@yahoo.com.br>
2023-11-26 23:57:07 -0300

    Página inicial criada apenas o básico, e pag login com estrutura criada mas ainda não está funcionando

--- estrutura.php
+++ estrutura.php
@@ -1,17 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>RGB</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    <link rel = "stylesheet" href = "css/style.css">
-</head>
-
-<body>
-<nav class = "barra"></nav>
-		<nav class="navbar navbar-expand-lg bg-dark" style="z-index: 2;">
+<nav class="navbar navbar-expand-lg bg-dark" style="z-index: 2;">
 			<div class="container-fluid">
 			  <a id="rgb" href="index.php" style=color:orange>
 				<span style="color:red">R</span>
@@ -136,10 +123,3 @@
             </ul>
         </div>
     </div>
-
-    <div class="main-content">
-
-    <div>
-    
-</body>
-</html>
---
+++ login.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RGB</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <link rel = "stylesheet" href = "css/style.css">
+    <?php include 'estrutura.php';?>
+</head>
+<body>
+    <div class="main-content">
+        <h1 style="padding: 10px 30px">Cadastrar-se</h1>
+        <br>
+		<a style="padding: 50px;color:rgb(109, 109, 109); padding-right: 0%; padding-left: 8%;" >Ao continuar você conconda com a <a href ="#" >política de privacidade</a></a>
+
+		<br>
+		<br>
+		<br>
+		<form action ="cadastrar.php" method="POST">
+			<p>
+			<center>
+				<input type="text" name="emailc" placeholder="E-mail" class="lblnormal">
+			</center>
+			</p>
+			<p>
+			<center>
+				<input type="text" name="usuario" placeholder="Usuário" class="lblnormal">
+			</center>
+			</p>
+			<center>
+				<input type="password" name="senhac" placeholder="Senha" class="lblnormal">
+			</center>
+			</p>
+			<br>
+			<br>
+			<p>
+			<center>
+				<button type="submit" class="btn-cadastrar" width="" id="entra">Cadastrar-se</button>
+			</center>
+			</p>
+		</form>
+
+		<?php if (isset($mensagem)) : ?>
+        <div class="alert">
+            <p><?php echo $mensagem; ?></p>
+            <button onclick="fecharMensagem()">Fechar</button>
+        </div>
+		<?php endif; ?>
+		<script>
+			function fecharMensagem() {
+				document.querySelector('.alert').style.display = 'none';
+			}
+		</script>
+
+		
+		<h1 style="padding: 10px 30px">Entrar</h1>
+		
+		<form action ="" method="POST">
+			<p>
+			<center>
+				<input type="text" name="email" placeholder="E-mail" class="lblnormal">
+			</center>
+			</p>
+			<center>
+				<input type="password" name="senha" placeholder="Senha" class="lblnormal">
+			</center>
+			</p>
+			<br>
+			<br>
+			<p>
+			<center>
+				<button type="submit" class="btn-cadastrar" id="entra">Entrar</button>
+			</center>
+			</p>
+		</form>
+		
+		<br>
+		<br>
+		<a id="esqc">Esqueceu a <a href ="esqueci.php">senha?</a></a>
+		<br>
+		<br>
+    </div>
+    
+</body>
+</html>
